c1b0f1faba423869e429c304fddf5ef7053a21c9
Jacky Zhang <jacky.zsn@gmail.com>
2021-08-14 23:19:59 -0400

    add toString method for messageBean

--- src/main/java/com/jszsoft/chatroombackend/model/MessageBean.java
+++ src/main/java/com/jszsoft/chatroombackend/model/MessageBean.java
@@ -2,9 +2,11 @@ package com.jszsoft.chatroombackend.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class MessageBean {
     private String type;
     private String name;
